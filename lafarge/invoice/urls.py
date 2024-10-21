96d2bc4e7ccdd84b5196bf2f337ccfa16c788c83
mfcc168 <88373848+mfcc168@users.noreply.github.com>
2024-10-21 11:18:13 +0800

    update

--- lafarge/invoice/urls.py
+++ lafarge/invoice/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import invoice_detail, download_invoice_pdf
+from .views import invoice_detail, download_invoice_pdf, customer_list
 
 urlpatterns = [
+    path('customers', customer_list, name='customer_list'),
     path('<str:invoice_number>', invoice_detail, name='invoice_detail'),
     path('<str:invoice_number>/download/', download_invoice_pdf, name='download_invoice_pdf'),
 ]
