3254635fe786c780c2b8005446ec495f3e29b90a
mfcc168 <88373848+mfcc168@users.noreply.github.com>
2025-01-13 15:32:18 +0800

    Update settings.py

--- lafarge/lafarge/settings.py
+++ lafarge/lafarge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@43pt-lfbrsxt@!nm)qnczn3$4@5(ua=aik3$5ou2@8ft_c&er
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.180.229.249', '127.0.0.1']
+ALLOWED_HOSTS = ['mfcc.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
