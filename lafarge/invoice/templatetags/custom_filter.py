cf54d98ad3e299ef52a1052e8bbfb8f46b2654bf
mfcc168 <88373848+mfcc168@users.noreply.github.com>
2025-02-04 15:22:18 +0800

    display price in currency format

---
+++ lafarge/invoice/templatetags/custom_filter.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='currency')
+def currency(value):
+    try:
+        value = float(value)
+        return f"{value:,.2f}"
+    except (ValueError, TypeError):
+        return value
